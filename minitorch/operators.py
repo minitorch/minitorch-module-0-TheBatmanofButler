14274531cc7638111b2255cffeffe8f1def184a9
Ganesh Ravichandran <the.ganesh.ravichandran@gmail.com>
2024-10-28 20:20:25 +0000

    done

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -34,11 +34,60 @@ from typing import Callable, Iterable
 
 # TODO: Implement for Task 0.1.
 
-def mul(a, b):
+def mul(a: float, b: float) -> float:
     return a * b
 
-def sigmoid(x: float) -> float:
-    return 1 / (1 + math.exp(x)**(-x))
+def id(a: float) -> float:
+    return a
+
+def add(a: float, b: float) -> float:
+    return a + b
+
+def neg(a: float) -> float:
+    return -a
+
+def lt(a: float, b: float) -> float:
+    return a < b
+
+def eq(a: float, b: float) -> float:
+    return a == b
+
+def max(a: float, b: float) -> float:
+    return a if a > b else b
+
+def is_close(a: float, b: float) -> bool:
+    if abs(a - b) <= abs(a) / 100 or abs(a - b) <= abs(b) / 100:
+        return True
+
+    return False
+
+def sigmoid(a: float) -> float:
+    if a > 0:
+        return 1 / (1 + math.exp(-a))
+    else:
+        e_a = math.exp(a)
+        return e_a / (1 + e_a)
+
+def relu(a: float) -> float:
+    return (a + abs(a)) / 2
+
+def log(a: float) -> float:
+    return math.log(a)
+
+def exp(a: float) -> float:
+    return math.exp(a)
+
+def inv(a: float) -> float:
+    return 1 / a
+
+def log_back(a: float, b: float) -> float:
+    return inv(a) * b
+
+def inv_back(a: float, b: float) -> float:
+    return -b / a ** 2
+
+def relu_back(a: float, b: float) -> float:
+    return b if a > 0 else 0
 
 # ## Task 0.3
 
@@ -57,3 +106,34 @@ def sigmoid(x: float) -> float:
 
 
 # TODO: Implement for Task 0.3.
+
+def map(iterable: Iterable, func: Callable) -> Iterable:
+    return [func(a) for a in iterable]
+
+def zipWith(iterableA: Iterable, iterableB: Iterable, func: Callable) -> Iterable:
+    if len(iterableA) != len(iterableB):
+        raise ValueError("Iterables must be the same length.")
+
+    return [func(iterableA[index], iterableB[index]) for index, _ in enumerate(iterableA)]
+
+def reduce(iterable: Iterable, func: Callable) -> float:
+    if len(iterable) < 1:
+        return 0
+
+    result = iterable[0]
+    for a in iterable[1:]:
+        result = func(a, result)
+    
+    return result
+
+def negList(iterable: Iterable) -> Iterable:
+    return map(iterable, neg)
+
+def addLists(iterableA: Iterable, iterableB: Iterable) -> Iterable:
+    return zipWith(iterableA, iterableB, add)
+
+def sum(iterable: Iterable) -> float:
+    return reduce(iterable, add)
+
+def prod(iterable: Iterable) -> float:
+    return reduce(iterable, mul)
